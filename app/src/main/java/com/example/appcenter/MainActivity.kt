2b70d0934b5a64d6baf2195051703851288f759a
mohamad alemi <mohamad.alemicode@gmail.com>
2025-07-15 21:15:19 +0330

    Modifying Greeting Title

--- app/src/main/java/com/example/appcenter/MainActivity.kt
+++ app/src/main/java/com/example/appcenter/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             AppCenterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "This is Android Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
