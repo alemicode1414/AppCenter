8eaba1d8fb977eec7d99120e1f4e7858f5fbe50a
mohamad alemi <mohamad.alemicode@gmail.com>
2025-08-02 18:54:25 +0330

    feat: Add feature

--- app/src/main/java/com/example/appcenter/MainActivity.kt
+++ app/src/main/java/com/example/appcenter/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appcenter.ui.theme.AppCenterTheme
@@ -29,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppCenterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "This is Android Test",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting("Hello world")
                 }
             }
         }
